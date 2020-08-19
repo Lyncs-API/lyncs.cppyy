fb52353443e4af848f8aa03312b3272e50da9792
Simone Bacchio <s.bacchio@gmail.com>
2020-08-19 15:26:25 +0300

    Adding test dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from lyncs_setuptools import setup
 
 setup(
-    "lyncs_cppyy", install_requires=["cppyy", "numpy"], keywords=["Lyncs", "cppyy",],
+    "lyncs_cppyy",
+    install_requires=["cppyy", "numpy"],
+    keywords=["Lyncs", "cppyy",],
+    extras_require={"test": ["pytest", "pytest-cov", "meson", "ninja",]},
 )
