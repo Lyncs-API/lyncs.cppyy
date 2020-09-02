e307451b777f341a68b1b3d7b7fd07eaf610d87f
Simone Bacchio <s.bacchio@gmail.com>
2020-09-02 15:19:49 +0000

    Applying black formatting (from Github Action)

--- setup.py
+++ setup.py
@@ -4,7 +4,17 @@ from lyncs_setuptools import setup
 setup(
     "lyncs_cppyy",
     install_requires=["cppyy", "numpy"],
-    keywords=["Lyncs", "cppyy",],
-    extras_require={"test": ["pytest", "pytest-cov", "meson", "ninja",]},
+    keywords=[
+        "Lyncs",
+        "cppyy",
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-cov",
+            "meson",
+            "ninja",
+        ]
+    },
     data_files=[("test/cnumbers", glob.glob("test/cnumbers/*"))],
 )
