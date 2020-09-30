de168ebde6b2610854a2b91fa14b7d59f69f99ad
Simone Bacchio <s.bacchio@gmail.com>
2020-09-30 12:41:02 +0300

    Fix issue

--- lyncs_cppyy/__init__.py
+++ lyncs_cppyy/__init__.py
@@ -5,7 +5,7 @@ In this package we provide some additional tools for the usage of
 [cppyy](https://cppyy.readthedocs.io/en/latest/) in the Lyncs API.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from cppyy import nullptr, cppdef, gbl
 from .lib import *
